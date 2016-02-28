581acb6f7ec3c5dfd7bb49eb31efb228560c1cf0
autyan <autyan@hotmail.com>
2016-02-29 02:39:04 +0800

    临时提交

--- Platform.Process/IProcess/IProcessBase.cs
+++ Platform.Process/IProcess/IProcessBase.cs
@@ -1,7 +1,49 @@
-﻿namespace SHWD.Platform.Process.IProcess
-{
-    public interface IProcessBase
-    {
+﻿using System;
+using System.Collections.Generic;
 
+namespace SHWD.Platform.Process.IProcess
+{
+    public interface IProcessBase<T> where T: class 
+    {
+        /// <summary>
+        /// 获取所有模型对象
+        /// </summary>
+        /// <returns>所有模型对象</returns>
+        IEnumerable<T> GetModels();
+
+        /// <summary>
+        /// 获取指定模型对象
+        /// </summary>
+        /// <param name="exp">查询条件表达式</param>
+        /// <returns>符合查询条件的对象</returns>
+        IEnumerable<T> GetModels(Func<T, bool> exp);
+
+        /// <summary>
+        /// 添加或修改对象
+        /// </summary>
+        /// <param name="model">被添加或修改的对象</param>
+        /// <returns>操作成功返回True，失败返回False</returns>
+        bool AddOrUpdate(T model);
+
+        /// <summary>
+        /// 批量添加对象
+        /// </summary>
+        /// <param name="models">被添加或修改的对象列表</param>
+        /// <returns>成功添加或修改的对象数量</returns>
+        int AddOrUpdate(IEnumerable<T> models);
+
+        /// <summary>
+        /// 删除对象
+        /// </summary>
+        /// <param name="model">被删除的对象</param>
+        /// <returns>操作成功返回True，失败返回False</returns>
+        bool Delete(T model);
+
+        /// <summary>
+        /// 批量删除对象
+        /// </summary>
+        /// <param name="models">被删除的对象列表</param>
+        /// <returns>成功删除的对象数量</returns>
+        int Delete(IEnumerable<T> models);
     }
 }
--- Platform.Process/Process/ProcessBase.cs
+++ Platform.Process/Process/ProcessBase.cs
@@ -1,11 +1,76 @@
-﻿using System.Runtime.InteropServices;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using SHWD.Platform.Process.Entities;
 using SHWD.Platform.Process.IProcess;
 
 namespace SHWD.Platform.Process.Process
 {
-    public class ProcessBase : IProcessBase
+    public class ProcessBase<T> : IProcessBase<T> where T : class
     {
-        
+        public IEnumerable<T> GetModels()
+        {
+            using (var context = new ProcessContext())
+            {
+                return context.Set<T>().ToList();
+            }
+        }
+
+        public IEnumerable<T> GetModels(Func<T, bool> exp)
+        {
+            using (var context = new ProcessContext())
+            {
+                return context.Set<T>().Where(exp).ToList();
+            }
+        }
+
+        public bool AddOrUpdate(T model)
+        {
+            using (var context = new ProcessContext())
+            {
+                if (context.Set<T>().Find(model) == null)
+                {
+                    context.Set<T>().Add(model);
+                }
+
+                return context.SaveChanges() == 1;
+            }
+        }
+
+        public int AddOrUpdate(IEnumerable<T> models)
+        {
+            using (var context = new ProcessContext())
+            {
+                foreach (var model in models)
+                {
+                    AddOrUpdate(model);
+                }
+
+                return context.SaveChanges();
+            }  
+        }
+
+        public bool Delete(T model)
+        {
+            using (var context = new ProcessContext())
+            {
+                context.Set<T>().Remove(model);
+
+                return context.SaveChanges() == 1;
+            }
+        }
+
+        public int Delete(IEnumerable<T> models)
+        {
+            using (var context = new ProcessContext())
+            {
+                foreach (var model in models)
+                {
+                    Delete(model);
+                }
+
+                return context.SaveChanges();
+            }
+        }
     }
 }
