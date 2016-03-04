142ea18ade012b5dedcb4ef90276ee17a42230b6
autyan <autyan@hotmail.com>
2016-03-04 23:41:31 +0800

    更新WEB组件

--- Web_Cloud_Platform/Controllers/HomeController.cs
+++ Web_Cloud_Platform/Controllers/HomeController.cs
@@ -1,17 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
+﻿using System.Web.Mvc;
 
 namespace Web_Cloud_Platform.Controllers
 {
     public class HomeController : Controller
     {
-        public ActionResult Index()
-        {
-            return View();
-        }
+        public ActionResult Index() => View();
 
         public ActionResult About()
         {
