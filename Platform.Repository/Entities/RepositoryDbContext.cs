08c4eb6c58ef0394a58056c99d28c94b84582570
Alex <autyan@outlook.com>
2016-03-15 19:34:56 +0800

    添加数据库连接字符串

--- Platform.Repository/Entities/RepositoryDbContext.cs
+++ Platform.Repository/Entities/RepositoryDbContext.cs
@@ -12,7 +12,7 @@ namespace SHWD.Platform.Repository.Entities
         /// <summary>
         /// 创建默认的DbContext
         /// </summary>
-        public RepositoryDbContext() : base("DefaultConnection")
+        public RepositoryDbContext() : base("name=Wd_Cloud_Platfrom")
         {
         }
 
