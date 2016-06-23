6ece6a1289edab241b96d25eb4b9980e783aa8e7
Alex <autyan@hotmail.com>
2016-06-23 19:13:41 +0800

    修改页面布局

---
+++ Lampblack_Platform/Controllers/ManagementController.cs
@@ -0,0 +1,29 @@
+﻿using System.Web.Mvc;
+using MvcWebComponents.Controllers;
+
+namespace Lampblack_Platform.Controllers
+{
+    public class ManagementController : WdControllerBase
+    {
+        // GET: Management
+        public ActionResult Area()
+        {
+            return View();
+        }
+
+        public ActionResult CateringEnterprise()
+        {
+            return View();
+        }
+
+        public ActionResult Hotel()
+        {
+            return View();
+        }
+
+        public ActionResult Device()
+        {
+            return View();
+        }
+    }
+}
