903afda2aa5d624678f2d69dd93f3286f8a6beb0
Misaka <autyan@hotmail.com>
2016-07-14 00:04:53 +0800

    小小调整

--- Lampblack_Platform/Scripts/Page/Map.js
+++ Lampblack_Platform/Scripts/Page/Map.js
@@ -10,6 +10,6 @@
 
     window.zoom = 12;
     var load = document.createElement("script");
-    load.src = "http://api.map.baidu.com/api?v=1.4&callback=map_init(getHotelInfo)";
+    load.src = "http://api.map.baidu.com/api?v=1.4&callback=map_init()";
     document.body.appendChild(load);
 })
