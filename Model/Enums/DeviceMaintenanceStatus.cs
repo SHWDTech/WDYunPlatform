314b66ab3a119443f1bf5c26e701ed3ea30493f7
Alex <autyan@hotmail.com>
2016-08-12 11:11:49 +0800

    添加设备维护对象模型

---
+++ Model/Enums/DeviceMaintenanceStatus.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SHWDTech.Platform.Model.Enums
+{
+    /// <summary>
+    /// 设备维护状态
+    /// </summary>
+    public enum DeviceMaintenanceStatus
+    {
+        [Display(Name = "很脏")]
+        Dirty,
+
+        [Display(Name = "一般")]
+        Normal,
+
+        [Display(Name = "干净")]
+        Clean
+    }
+}
---
+++ Platform.Repository/IRepository/IDeviceMaintenanceRepository.cs
@@ -0,0 +1,8 @@
+﻿using SHWDTech.Platform.Model.Model;
+
+namespace SHWD.Platform.Repository.IRepository
+{
+    public interface IDeviceMaintenanceRepository : IRepository<DeviceMaintenance>
+    {
+    }
+}
