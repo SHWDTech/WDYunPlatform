53fc15f1ec124b0cdfe94629927040c5b04d443e
Alex <autyan@hotmail.com>
2016-08-30 18:16:35 +0800

    添加自动数据模块

---
+++ LampblackTransfer/DeviceInfo.cs
@@ -0,0 +1,11 @@
+﻿namespace LampblackTransfer
+{
+    public class DeviceInfo
+    {
+        public string NodeId { get; set; }
+
+        public bool Opened { get; set; }
+
+        public byte Rate { get; set; }
+    }
+}
---
+++ LampblackTransfer/ModelRange.cs
@@ -0,0 +1,13 @@
+﻿namespace LampblackTransfer
+{
+    public class ModelRange
+    {
+        public static int Fail = 5000;
+
+        public static int Worse = 20000;
+
+        public static int Qualified = 50000;
+
+        public static int Good = 50001;
+    }
+}
