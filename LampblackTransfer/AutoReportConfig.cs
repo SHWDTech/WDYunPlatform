ff933fd8b998fd7135726ba878e91eb7e09b4339
Alex <autyan@hotmail.com>
2016-08-30 18:16:35 +0800

    添加自动数据模块

---
+++ LampblackTransfer/AutoReportConfig.cs
@@ -0,0 +1,13 @@
+﻿namespace LampblackTransfer
+{
+    public class AutoReportConfig
+    {
+        public string NodeId { get; set; }
+
+        public bool CleanerSwitch { get; set; }
+
+        public int CleanerNumber { get; set; }
+
+        public bool FanSwitch { get; set; }
+    }
+}
--- LampblackTransfer/Program.cs
+++ LampblackTransfer/Program.cs
@@ -1,9 +1,18 @@
-﻿namespace LampblackTransfer
+﻿using System.Collections.Generic;
+
+namespace LampblackTransfer
 {
     class Program
     {
+        private List<DeviceInfo> _deviceInfos = new List<DeviceInfo>();
+
         static void Main(string[] args)
         {
+        }
+
+        static void InitDeviceInfos()
+        {
+            
         }
     }
 }
