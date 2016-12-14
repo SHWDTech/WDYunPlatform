6be2838a9582d137b2dbc30fac2aab0525c8b2b0
Alex <autyan@hotmail.com>
2016-12-14 17:09:50 +0800

    添加LONGMODEL模型

---
+++ SHWDTech.Platform.StorageConstrains/Model/LongModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using SHWDTech.Platform.StorageConstrains.Model.Enums;
+
+namespace SHWDTech.Platform.StorageConstrains.Model
+{
+    [Serializable]
+    public class LongModel : ILongModel
+    {
+        [Key]
+        public virtual Guid Id { get; set; }
+
+        public virtual ModelState ModelState { get; set; }
+
+        public virtual bool IsNew => ModelState == ModelState.Added;
+    }
+}
