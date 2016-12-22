69ef1bef15f01ea850b8bf3683e0fafd23b30468
Alex <autyan@hotmail.com>
2016-12-22 17:08:14 +0800

    添加加密算法。

---
+++ Platform.Utility/ExtensionMethod/RSACryptoServiceProviderExtensionMethod.cs
@@ -0,0 +1,38 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+
+namespace SHWDTech.Platform.Utility.ExtensionMethod
+{
+    public static class RsaCryptoServiceProviderExtensionMethod
+    {
+        public static string DecryptString(this RSACryptoServiceProvider rsaCryptoServiceProvider, string sourceString)
+        {
+            var byteEn = rsaCryptoServiceProvider.Encrypt(Encoding.ASCII.GetBytes("a"), false);
+            var sBytes = sourceString.Split(',');
+
+            for (var j = 0; j < sBytes.Length; j++)
+            {
+                if (sBytes[j] != "")
+                {
+                    byteEn[j] = byte.Parse(sBytes[j]);
+                }
+            }
+            var plaintbytes = rsaCryptoServiceProvider.Decrypt(byteEn, false);
+            return Encoding.ASCII.GetString(plaintbytes);
+        }
+
+        public static string EncryptString(this RSACryptoServiceProvider rsaCryptoServiceProvider, string sourceString)
+        {
+            var plaintext = sourceString;
+            var cipherbytes = rsaCryptoServiceProvider.Encrypt(Encoding.ASCII.GetBytes(plaintext), false);
+
+            var sbString = new StringBuilder();
+            foreach (var t in cipherbytes)
+            {
+                sbString.Append(t + ",");
+            }
+
+            return sbString.ToString();
+        }
+    }
+}
---
+++ Platform.Utility/Serialize/XmlSerializerHelper.cs
@@ -0,0 +1,23 @@
+﻿using System.IO;
+using System.Xml.Serialization;
+
+namespace SHWDTech.Platform.Utility.Serialize
+{
+    public class XmlSerializerHelper
+    {
+        public static string Serialize<T>(T target)
+        {
+            var stringwriter = new StringWriter();
+            var serializer = new XmlSerializer(typeof(T));
+            serializer.Serialize(stringwriter, target);
+            return stringwriter.ToString();
+        }
+
+        public static T DeSerialize<T>(string jsonString)
+        {
+            var stringReader = new StringReader(jsonString);
+            var serializer = new XmlSerializer(typeof(T));
+            return (T)serializer.Deserialize(stringReader);
+        }
+    }
+}
