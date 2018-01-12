b8c4baf6ed3e247a904c211bf54b2d8bc3825a68
汤学地 <tangxuedi5206@outlook.com>
2018-01-12 13:49:15 +0800

    添加异常捕获。

--- Lampblack_Platform/Controllers/EnterpriseInfoController.cs
+++ Lampblack_Platform/Controllers/EnterpriseInfoController.cs
@@ -6,6 +6,7 @@ using MvcWebComponents.Controllers;
 using Platform.Process.Process;
 using SHWD.Platform.Repository.Entities;
 using SHWDTech.Platform.Model.Enums;
+using SHWDTech.Platform.Utility;
 
 namespace Lampblack_Platform.Controllers
 {
@@ -14,50 +15,58 @@ namespace Lampblack_Platform.Controllers
     {
         public EnterpriseInfo Get([FromUri]string domain)
         {
-            var context = new RepositoryDbContext();
-            var dics = context.SysDictionaries.Where(d => d.ItemName == "HuangpuPlatform").ToList();
-            var domainId = Guid.Parse(dics.First(d => d.ItemKey == $"{domain.ToUpper()}DomainId").ItemValue);
-            var prefix = dics.First(d => d.ItemKey == $"{domain.ToUpper()}Prefix").ItemValue;
-            var area = ProcessInvoke<UserDictionaryProcess>().GetAreaByName("黄浦区");
-            var model = new EnterpriseInfo();
-            var devsGroup = ProcessInvoke<RestaurantDeviceProcess>()
-                .DevicesInDistrict(area.Id, device => device.DomainId == domainId && device.Status == DeviceStatus.Enabled)
-                .OrderBy(d => d.Identity)
-                .GroupBy(dev => dev.Hotel);
-            foreach (var group in devsGroup)
+            try
             {
-                var ordered = group.OrderBy(d => d.Identity);
-                var alpha = 65;
-                int? endfix = null;
-                if (group.Count() > 1) endfix = 1;
-                foreach (var dev in ordered)
+                var context = new RepositoryDbContext();
+                var dics = context.SysDictionaries.Where(d => d.ItemName == "HuangpuPlatform").ToList();
+                var domainId = Guid.Parse(dics.First(d => d.ItemKey == $"{domain.ToUpper()}DomainId").ItemValue);
+                var prefix = dics.First(d => d.ItemKey == $"{domain.ToUpper()}Prefix").ItemValue;
+                var area = ProcessInvoke<UserDictionaryProcess>().GetAreaByName("黄浦区");
+                var model = new EnterpriseInfo();
+                var devsGroup = ProcessInvoke<RestaurantDeviceProcess>()
+                    .DevicesInDistrict(area.Id, device => device.DomainId == domainId && device.Status == DeviceStatus.Enabled)
+                    .OrderBy(d => d.Identity)
+                    .GroupBy(dev => dev.Hotel);
+                foreach (var group in devsGroup)
                 {
-                    var enterp = new Enterprise
+                    var ordered = group.OrderBy(d => d.Identity);
+                    var alpha = 65;
+                    int? endfix = null;
+                    if (group.Count() > 1) endfix = 1;
+                    foreach (var dev in ordered)
                     {
-                        QYBM = $"{prefix}{dev.Hotel.Identity:D4}{endfix:D2}",
-                        QYMC = $"{dev.Hotel.RaletedCompany.CompanyName}({dev.Hotel.ProjectName})",
-                        QYDZ = dev.Hotel.AddressDetail,
-                        PER = dev.Hotel.ChargeMan,
-                        TEL = dev.Hotel.Telephone,
-                        QYSTREET = dev.Hotel.Street.ItemValue,
-                        XPOS = dev.Hotel.Longitude.ToString(),
-                        YPOS = dev.Hotel.Latitude.ToString(),
-                        CASE_ID = $"{prefix}{dev.Identity:D6}",
-                        CASE_NAM = $"{(char)alpha}设备箱"
-                    };
+                        var enterp = new Enterprise
+                        {
+                            QYBM = $"{prefix}{dev.Hotel.Identity:D4}{endfix:D2}",
+                            QYMC = $"{dev.Hotel.RaletedCompany.CompanyName}({dev.Hotel.ProjectName})",
+                            QYDZ = dev.Hotel.AddressDetail,
+                            PER = dev.Hotel.ChargeMan,
+                            TEL = dev.Hotel.Telephone,
+                            QYSTREET = dev.Hotel.Street.ItemValue,
+                            XPOS = dev.Hotel.Longitude.ToString(),
+                            YPOS = dev.Hotel.Latitude.ToString(),
+                            CASE_ID = $"{prefix}{dev.Identity:D6}",
+                            CASE_NAM = $"{(char)alpha}设备箱"
+                        };
 
-                    model.data.Add(enterp);
-                    alpha++;
-                    if (endfix != null)
-                    {
-                        endfix++;
+                        model.data.Add(enterp);
+                        alpha++;
+                        if (endfix != null)
+                        {
+                            endfix++;
+                        }
                     }
                 }
+
+                model.result = "success";
+
+                return model;
             }
-
-            model.result = "success";
-
-            return model;
+            catch (Exception ex)
+            {
+                LogService.Instance.Error("EnterpriseInfoError", ex);
+            }
+            return null;
         }
     }
 }
